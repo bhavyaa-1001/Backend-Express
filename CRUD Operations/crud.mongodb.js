2bdbcbb0abaf70aae82beb83dde150013e65f937
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-04-13 15:48:15 +0530

    CRUD

---
+++ CRUD Operations/crud.mongodb.js
@@ -0,0 +1,15 @@
+use("CrudDb")
+
+// console.log(db)
+
+db.createCollection("courses")
+db.courses.insertOne({
+    name: "Bhavya",
+    age: 22,
+    assignments: [
+        { title: "Assignment 1", score: 85 },
+        { title: "Assignment 2", score: 90 },
+        { title: "Assignment 3", score: 78 }
+    ],
+    completed: false,
+})
