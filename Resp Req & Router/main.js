e97548e237fdba2f7741e4e72aefbe834468ec46
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-07-17 15:45:33 +0530

    e

--- Resp Req & Router/main.js
+++ Resp Req & Router/main.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/index', (req, res) => {
+  res.sendFile('templates/index.html', { root: __dirname })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
